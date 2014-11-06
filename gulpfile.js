d758d8b59bc77450748cae1357b20497f3fb7c27
Mihai Scurtu <mscurtu90@gmail.com>
2014-11-06 14:42:11 +0200

    Add basic JS and gulp build;

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 gulp = require('gulp');
 less = require('gulp-less');
 minify = require('gulp-minify-css');
+concat = require('gulp-concat');
 sourcemaps = require('gulp-sourcemaps');
+merge = require('merge-stream');
+watch = require('gulp-watch');
 
 var paths = {
-  less: ['css/**/*.less']
+  less: ['css/**/*.less'],
+  js: ['js/src/**/*.js']
 }
 
 gulp.task('less', function() {
@@ -14,10 +18,23 @@ gulp.task('less', function() {
     .pipe(minify())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'));
-})
-
-gulp.task('watch', function() {
-  return gulp.watch(paths.less, ['less']);
 });
 
-gulp.task('default', ['watch', 'less']);
+gulp.task('build-js', function() {
+  return gulp.src(paths.js)
+    .pipe(concat('build.js'))
+    .pipe(minify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('watch', function() {
+  var less = gulp.src(paths.less)
+    .pipe(watch(paths.less), 'less');
+  var js = gulp.src(paths.js)
+    .pipe(watch(paths.js), 'build-js');
+
+  return merge(less, js);
+});
+
+gulp.task('default', ['watch', 'less', 'build-js']);
